395cfee96e8f3deaafb98e7c1e706e640d5d6f66
NavaFFG <90836555+NavaFFG@users.noreply.github.com>
2021-12-17 09:50:52 +0100

    Update views.py

--- data_view/views.py
+++ data_view/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import pandas as pd
 # Create your views here.
-def view(request):
+def views(request):
     if request.method == 'POST' and request.FILES['csv_file']:
         data = pd.read_csv(request.FILES['csv_file'], sep=',')
         print(data)
