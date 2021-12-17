c12a4217e26eb0cfdf1ef6891e15c37729fb9ab3
NavaFFG <90836555+NavaFFG@users.noreply.github.com>
2021-12-17 09:22:58 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,4 @@
+import django_heroku
+django_heroku.settings(locals())
+TEST_RUNNER = 'django_heroku.HerokuDiscoverRunner'
+SECRET_KEY = 'django-insecure-iiwv%u1sc0uotgps=+ak5ujya2wgj0k(3ugwv_+6i2342#j875'
